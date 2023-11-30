459e7564523267ae3571d493a9acdbc434e0742f
DavideGila <davide.gila@gmail.com>
2023-11-30 16:44:00 +0100

    completed till Milestone 3

--- login.php
+++ login.php
@@ -4,9 +4,19 @@ $error = generatePassword();
 ?>
 
 <main class="container">
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET" class='py-5'>
-        <input type="number" name="password_length">
-        <button type='submit'>Genera</button>
-        <button type='reset'>Reset</button>
-    </form>
+    <div class="bg-light">
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET" class='py-5'>
+            <div class="row">
+                <div class="col-8 ps-5">
+                    <div>Lunghezza password:</div>
+                    <div class="pt-3 pb-5">Consenti ripetizioni di uno o più caratteri:</div>
+                    <button type='submit'>Genera</button>
+                    <button type='reset'>Reset</button>
+                </div>
+                <div class="col-4">
+                    <input type="number" name="password_length">
+                </div>
+            </div>
+        </form>
+    </div>
 </main>
