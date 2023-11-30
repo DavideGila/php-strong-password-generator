4b06435690751655b572f9f5f6d468887e5337cb
DavideGila <davide.gila@gmail.com>
2023-11-30 14:45:29 +0100

    header updated

---
+++ partials/template/header.php
@@ -0,0 +1,18 @@
+<?php
+    include __DIR__ . "/../../Model/db.php";
+    include __DIR__ . "/../functions/functions.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+<body>
+    <header class="container">
+        <h1 class="text-center">Strong Password Generator</h1>
+        <h2 class="text-center">Genera una password sicura</h2>
+    </header>
